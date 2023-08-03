b835e0b9fe5ab51dfd8f44970d01edb18a75817a
ArielCereghetti <arielcereghetti@live.com>
2023-08-03 20:25:09 +0100

    Added A Register User form and a Login form to authenticate the users

--- CRM/forms.py
+++ CRM/forms.py
@@ -2,6 +2,56 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-class SignUpForm(UserCreationForm):
-    email = forms.EmailField(label="", widget=forms.TextInput())
-    name = forms.CharField()
+
+
+class RegisterUser(UserCreationForm):
+    class_attr = 'signupform'
+    username = forms.CharField(
+        label="Username",
+        widget=forms.TextInput(
+            attrs={
+                "class": class_attr,
+            })
+    )
+    email = forms.EmailField(
+        label="Email", 
+        widget=forms.EmailInput(
+            attrs={
+                "class": class_attr,
+            }),
+        max_length=170
+        )
+    password1 = forms.CharField(
+        label="Password", 
+        widget=forms.PasswordInput(
+            attrs={
+                "class": class_attr,
+                }), 
+        max_length=100
+        )
+    password2 = forms.CharField(
+        label="Repeat password",
+        widget=forms.PasswordInput(
+            attrs={
+                "class": class_attr,
+            }),
+        max_length=100,
+        )
+
+    class Meta():
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
+
+
+class LoginForm(forms.Form):
+    class_attr = 'signupform'
+    username = forms.CharField(max_length=100, label="Username", widget=forms.TextInput(
+        attrs={
+            'class': class_attr,
+            })
+        )
+    password = forms.CharField(max_length=100, label="Password", widget=forms.PasswordInput(
+        attrs={
+            'class': class_attr,
+        }
+    ))
