541cd10c4976421a4024462d54380c40d1de07b8
ArielCereghetti <arielcereghetti@live.com>
2023-08-03 20:31:54 +0100

    Updated some unused imports and added others

--- CRM/views.py
+++ CRM/views.py
@@ -1,5 +1,9 @@
+from django import forms
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+from .forms import RegisterUser, LoginForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
