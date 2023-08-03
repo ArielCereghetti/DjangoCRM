c5cdb464c13de10dd6edf218e31025049fc15a60
ArielCereghetti <arielcereghetti@live.com>
2023-08-03 20:33:21 +0100

    Added a logout view from django's views

--- CRM/urls.py
+++ CRM/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
 
@@ -7,5 +7,6 @@ app_name = "CRM"
 urlpatterns = [
     path("", views.index, name="index"),
     path("auth/login/", views.login_user, name="login"),
+    path("auth/logout/", auth_views.LogoutView.as_view(next_page='CRM:login'), name="logout"),
     path("auth/register/", views.register_user, name="register"),
 ]
