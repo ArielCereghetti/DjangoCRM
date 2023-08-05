1b9a73a8588eacd87ea9f2b8ee3c545b9df031ed
ArielCereghetti <arielcereghetti@live.com>
2023-08-05 15:05:48 +0100

    Implemented the index and upgraded the login

--- CRM/views.py
+++ CRM/views.py
@@ -1,16 +1,25 @@
 from django import forms
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
+from django.urls import reverse
 from .forms import RegisterUser, LoginForm
 from django.contrib.auth.models import User
+
+from .models import Customer
 
 # Create your views here.
 @login_required(login_url="CRM:login")
 def index(request):
-    return render(request, 'CRM/index.html')
+    
+
+    return render(request, 'CRM/index.html', {'model': Customer().__dict__()})
 
 def login_user(request):
+    if request.user.is_authenticated:
+        return HttpResponseRedirect(reverse('CRM:index'))
+
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
@@ -20,10 +29,10 @@ def login_user(request):
 
         if user:
             login(request, user)
+            return HttpResponseRedirect(reverse('CRM:index'))
         else:
             return render(request, 'CRM/login.html', {"form": form })
-
-
+    
 
     return render(request, 'CRM/login.html', {"form": LoginForm()})
 
